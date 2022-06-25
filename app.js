eafef75687655ae847c4897f989be8a7e5eba509
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-25 20:38:25 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
 const Btn = document.getElementById("btn");
 const card = document.getElementById("cardy");
-Btn.addEventListener("click", getNews);
-let stories = [];
+Btn.addEventListener("click", getAllNew);
 
-function getNews(e) {
-    let URL = "https://newsapi.org/v2/everything?q=ransomware"
+function getAllNew(){
+    getWarNews();
+    getCyberNews();
+}
+
+function getCyberNews(e) {
+    let URL = "https://newsapi.org/v2/everything?q=cyber(ransomware AND crypto)"
     fetch(URL, {
         method: "GET",
         headers: {
-            'Authorization': API_KEY
+            'Authorization': '477f3f4a86e2451198ba71e20dc9db29'
         }
     })
     .then(response => response.json())
     .then(response => {
         let res = response.articles;
-        // console.log(res);
+        console.log(res);
         res.forEach(story => {
-            console.log(story);
+            // console.log(story);
             card.innerHTML += `
-            <div class="card" style="width: 18rem;">
+            <div class="card col-sm" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title" id="newsTitle">${story.title}</h5>
                     <p class="card-text" id="newsSyn">${story.description}</p>
-                    <a href="{article.url}" class="btn btn-primary">Read More</a>
+                    <a href="${story.url}" class="btn btn-primary">Read More</a>
                 </div>
             </div>
             `;
         });
     });
 }
-// function getID() {
-//     fetch("newsapk.json")
-//     .then(response => response.json())
-//     .then(response => console.log(response.key))
-// }
-// const getKey = getID();
-// console.log(getKey);
+
+function getWarNews(e) {
+    let URL = "https://newsapi.org/v2/everything?q=war"
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            'Authorization': API_KEY'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        let res = response.articles;
+        console.log(res);
+        res.forEach(story => {
+            // console.log(story);
+            card.innerHTML += `
+            <div class="card col-sm" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title" id="newsTitle">${story.title}</h5>
+                    <p class="card-text" id="newsSyn">${story.description}</p>
+                    <a href="${story.url}" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            `;
+        });
+    });
+}
+
+
+
