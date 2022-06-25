98e2fb3a2505c026d8a4dbead9c719f16732e9b9
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-25 20:39:02 +0000

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function getCyberNews(e) {
     fetch(URL, {
         method: "GET",
         headers: {
-            'Authorization': '477f3f4a86e2451198ba71e20dc9db29'
+            'Authorization': API_KEY
         }
     })
     .then(response => response.json())
