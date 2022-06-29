3cda9e0a705e516c4ab3f38e8ac272281b5fd26b
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-29 02:20:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const card = document.getElementById("container");
 const cyberBtn = document.getElementById("cyberLink");
 const warBtn = document.getElementById("warLink");
 
-cyberBtn.addEventListener("click", getCyberNews);
 warBtn.addEventListener("click", getWarNews);
+cyberBtn.addEventListener("click", getCyberNews);
+
 
 function getCyberNews(e) {
+    let URL = "https://newsapi.org/v2/everything?q=cyber(ransomware AND crypto)";
+    getNews(URL);
+}
+
+function getWarNews(e) {
+    let URL = "https://newsapi.org/v2/everything?q=war";
+    getNews(URL);
+}
+
+
+function getNews(URL){
     card.innerHTML = "";
-    let URL = "https://newsapi.org/v2/everything?q=cyber(ransomware AND crypto)"
     fetch(URL, {
         method: "GET",
         headers: {
-            'Authorization': 'APIKEY'
+            'Authorization': 'API KEY'
         }
     })
     .then(response => response.json())
@@ -22,40 +33,10 @@ function getCyberNews(e) {
                 <div id="card">
                     <h2>${story.title}</h2>
                     <p>${story.description}</p>
-                    <a href="#" id="btn-link">${story.url}</a>
+                    <a href="${story.url}" id="btn-link">Read More</a>
                     </div>
                 </div>
             `;
         });
     });
 }
-
-function getWarNews(e) {
-    card.innerHTML = "";
-    let URL = "https://newsapi.org/v2/everything?q=war"
-    fetch(URL, {
-        method: "GET",
-        headers: {
-            'Authorization': APIKEY
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        let res = response.articles;
-        console.log(res);
-        res.forEach(story => {
-            // console.log(story);
-            card.innerHTML += `
-            <div id="card">
-                    <h2>${story.title}</h2>
-                    <p>${story.description}</p>
-                    <a href="#" id="btn-link">${story.url}</a>
-                    </div>
-                </div>
-            `;
-        });
-    });
-}
-
-
-
